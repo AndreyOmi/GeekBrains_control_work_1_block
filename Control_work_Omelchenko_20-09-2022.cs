046509b5308c426f594c9bc9b88bd8ab4df8d6f7
Andrey_Omelchenko <omi@bk.ru>
2022-09-20 18:47:01 +0300

    Adding to project whole text of programm

--- Control_work_Omelchenko_20-09-2022.cs
+++ Control_work_Omelchenko_20-09-2022.cs
@@ -16,3 +16,53 @@ void print_array_strings(string[] Input_Array)
 
 // Конец области определения функций
 
+int Goal_words_counter = 0;
+
+Console.Write("Введите массив слов через пробел: ");
+
+string Variables_str = Console.ReadLine();  
+
+string[] words = Variables_str.Split(' ');
+
+/*
+Чтобы не использовать модуль коллекций с динамическими массивами
+на первом этапе проводится подсчет числа слов, состоящих из трех 
+или меньшего числа знаков.
+*/
+
+foreach(string word in words)
+      {
+       if(word.Length <= 3)
+        Goal_words_counter++;
+      }
+
+if(Goal_words_counter == 0)
+ {
+  Console.WriteLine();
+  Console.WriteLine("Исходный массив не содержит строк, длина которых меньше или равна трём символам!");
+ } else 
+      {
+       /*
+        На втором этапе значение количества слов, состоящих из трех 
+        или меньшего числа знаков, используется для инициализации
+        обычного статического массива слов, который заполняется
+        указанными словами.
+       */
+       
+       string[] Goal_words = new string[Goal_words_counter]; 
+
+       int Goal_words_index = 0;
+
+       foreach(string word in words)
+             {
+              if(word.Length <= 3)
+               {
+                Goal_words[Goal_words_index] = word;
+                Goal_words_index++;
+               }
+             }
+
+       Console.WriteLine();
+       Console.Write("Исходный массив содержит следующие строки, длина которых меньше или равна трём символам: ");
+       print_array_strings(Goal_words);
+      }
