c6b65318c8e995eca2fadc0076df938daa43535b
Andrey_Omelchenko <omi@bk.ru>
2022-09-20 18:42:27 +0300

    Adding to project a function for printing of string array

---
+++ Control_work_Omelchenko_20-09-2022.cs
@@ -0,0 +1,18 @@
+﻿/*
+Итоговая контрольная работа по первому блоку обучения 
+Исполнитель: Омельченко А.В.
+*/
+
+// Определение функции для печати массива строк
+
+void print_array_strings(string[] Input_Array)
+   {
+    foreach(string element in Input_Array)
+          {
+           Console.Write(element);
+           Console.Write(" ");
+          }
+   }
+
+// Конец области определения функций
+
